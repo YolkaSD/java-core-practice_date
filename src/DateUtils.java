6470fd2789af4ecfcd55dec504e50e49698dcfb1
Slava Rybkin <yolka.sd@gmail.com>
2023-03-19 17:27:38 +0700

    Completed practice

---
+++ src/DateUtils.java
@@ -0,0 +1,109 @@
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class DateUtils {
+    //private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    /**
+     * @dateTimeFormatters - Список объектов DateTimeFormatter, содержащий предопределенные форматы даты и времени.
+     */
+    private final static List<DateTimeFormatter> DATE_TIME_FORMATTERS = new ArrayList<>();
+
+    static {
+        DATE_TIME_FORMATTERS.add(DateTimeFormatter.ofPattern("dd MM yyyy"));
+        DATE_TIME_FORMATTERS.add(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        DATE_TIME_FORMATTERS.add(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        DATE_TIME_FORMATTERS.add(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        DATE_TIME_FORMATTERS.add(DateTimeFormatter.ofPattern("yyyy MM dd"));
+        DATE_TIME_FORMATTERS.add(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+        DATE_TIME_FORMATTERS.add(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        DATE_TIME_FORMATTERS.add(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    }
+
+    /**
+     * Метод возвращает количество дней между днем рождения и текущей датой в формате int.
+     *
+     * @param birthdayDateString строка, содержащая дату рождения в формате dd-MM-yyyy
+     * @return количество дней между днем рождения и текущей датой
+     * @throws InvalidDateException если строка содержит неверный формат даты или дата рождения находится в будущем
+     */
+    public static int getDaysSinceBirthday(String birthdayDateString) throws InvalidDateException {
+        if (birthdayDateString == null) {
+            throw new InvalidDateException("Birthday date string is null");
+        }
+        try {
+            LocalDate birthdayDate = LocalDate.parse(birthdayDateString, DATE_TIME_FORMATTERS.get(2));
+            LocalDate today = LocalDate.now();
+
+            int days = (int) ChronoUnit.DAYS.between(birthdayDate, today);
+
+            if (birthdayDate.isAfter(today)) {
+                throw new InvalidDateException("Birthday date is in the future: " + birthdayDateString + ". The date should be in format dd-MM-yyyy.");
+            }
+
+            return days;
+        } catch (DateTimeParseException e) {
+            throw new InvalidDateException("Invalid date format: " + birthdayDateString + ". The date should be in format dd-MM-yyyy.");
+        }
+    }
+
+    /**
+     * Парсит строку даты в неизвестном формате и возвращает объект LocalDate.
+     *
+     * @param dateString строка, содержащая дату в неизвестном формате
+     * @return объект LocalDate, если формат даты соответствует любому из форматов, указанных в списке
+     * dateTimeFormatters, в противном случае null
+     * @throws DateTimeParseException, если строку даты нельзя разобрать ни одним из
+     */
+    public static LocalDate parseDateFromString(String dateString) {
+        for (DateTimeFormatter formatter : DATE_TIME_FORMATTERS) {
+            try {
+                return LocalDate.parse(dateString, formatter);
+            } catch (DateTimeParseException e) {
+                System.err.println("Error parsing date string: " + dateString + " with formatter: " + formatter.toString() + "Error message: " + e.getMessage());
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Конвертирует время из одной временной зоны в другую.
+     * @param zonedDateTime исходное время в виде объекта ZonedDateTime, не может быть null
+     * @param targetZoneId целевая временная зона в виде объекта ZoneId, не может быть null
+     * @return время в целевой временной зоне в виде объекта ZonedDateTime
+     * @throws NullPointerException если zonedDateTime или targetZoneId равны null
+     */
+    public static ZonedDateTime convertTime(ZonedDateTime zonedDateTime, ZoneId targetZoneId) {
+        Objects.requireNonNull(zonedDateTime, "zonedDateTime parameter cannot be null");
+        Objects.requireNonNull(targetZoneId, "targetZoneId parameter cannot be null");
+        return zonedDateTime.withZoneSameInstant(targetZoneId);
+    }
+
+    /**
+     * Метод findMaxDate находит наиболее старшую дату из листа localDateList
+     * @param localDateList содержит в себе список дат
+     * @return возвращает наиболее старшую дату
+     * @throws IllegalArgumentException Если список localDateList пустой.
+     */
+    public static LocalDateTime findMaxDate(List<LocalDateTime> localDateList) {
+        return localDateList.stream()
+                .max(LocalDateTime::compareTo)
+                .orElseThrow(() -> new IllegalArgumentException("List cannot be empty"));
+    }
+
+    static class InvalidDateException extends Exception {
+        public InvalidDateException(String message) {
+            super(message);
+        }
+
+        @Override
+        public String toString() {
+            return getMessage();
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,94 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
+import java.time.*;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        String dayString = "15-12-2002";
-        int daysFromBirthday = DaysFromBirthday(dayString);
-        System.out.println(daysFromBirthday);
-    }
+        //Task - 1
+        String dayString = "14-01-2007";
+        try {
+            System.out.println("Task-1: Прошло дней со дя рождения \"14 - 01 - 2007\": " +
+                    DateUtils.getDaysSinceBirthday(dayString));
+        } catch (DateUtils.InvalidDateException e) {
+            throw new RuntimeException(e);
+        }
+        //Task - 2
+        dayString = "15 12 2002";
+        System.out.println("Task-2: " + DateUtils.parseDateFromString(dayString));
+        dayString = "15 12-2002";
+        System.out.println("Task-2: " + DateUtils.parseDateFromString(dayString));
 
-    //Написать метод, который будет принимать дату рождения в строке и возвращать количество дней в формате инт,
-    // которые прошли с этого дня.
-    public static int DaysFromBirthday(String birthday) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        LocalDate birthdayDate = LocalDate.parse(birthday, formatter);
-        LocalDate nowDate = LocalDate.now();
-        return (int) ChronoUnit.DAYS.between(birthdayDate, nowDate);
+        //Task - 3
+        ZonedDateTime zonedDateTime = ZonedDateTime.now().withZoneSameInstant(ZoneId.of("Europe/Kiev"));
+        ZoneId kiev = ZoneId.of("Europe/Kiev");
+        LocalTime kievTime = DateUtils.convertTime(zonedDateTime, kiev).toLocalTime();
+        System.out.println("Task - 3: " + kievTime.getHour() + ":" + kievTime.getMinute());
+
+        //Task - 4
+        long start = System.currentTimeMillis();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {
+        }
+        long finish = System.currentTimeMillis();
+        System.out.println("Task - 4: " + (double) (finish - start) / 1000);
+
+        //Task - 5
+        Random randon = new Random();
+        List<LocalDateTime> localDateTimes = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            int year = randon.nextInt(300) + 1800;
+            Month month = Month.of(randon.nextInt(12) + 1);
+            int dayOfMonth = randon.nextInt(month.maxLength()) + 1;
+            int hour = randon.nextInt(24);
+            int minutes = randon.nextInt(60);
+
+            localDateTimes.add(LocalDateTime.of(year, month, dayOfMonth, hour, minutes));
+        }
+
+        System.out.println("Task - 5: max date " + DateUtils.findMaxDate(localDateTimes));
+
+        //Task - 6
+        LocalDate localDateTest1 = LocalDate.now();
+        LocalDate localDateTest2 = LocalDate.now();
+        //находится ли объект времени, у которого был вызван данный метод, после другого объекта времени, переданного в качестве аргумента
+        System.out.println(localDateTest1.isAfter(localDateTest2));
+        //
+        System.out.println(localDateTest1.plusDays(12));
+
+        //предназначенный для создания нового экземпляра объекта Temporal с измененным значением.
+        LocalDate newLocalDate = localDateTest1.with(ChronoField.YEAR, 2007);
+        System.out.println(newLocalDate);
+        //
+        System.out.println(localDateTest1.plus(Period.ofMonths(2)));
+
+        //get
+        System.out.println(localDateTest1.getEra());
+        System.out.println(localDateTest1.getChronology());
+        System.out.println(localDateTest1.getDayOfMonth());
+        System.out.println(localDateTest1.getYear());
+        //позволяет получить значение конкретного поля времени из этого объекта.
+        System.out.println(localDateTest1.get(ChronoField.DAY_OF_MONTH));
+        System.out.println(localDateTest1.get(ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH));
+        System.out.println(localDateTest1.getDayOfWeek());
+        System.out.println(localDateTest1.getDayOfYear());
+        System.out.println(localDateTest1.getMonthValue());
+        //
+        System.out.println(localDateTest1.atStartOfDay());
+        LocalDate startDate = LocalDate.now();
+        LocalDate endDate = startDate.plusDays(7);
+        //
+        Stream<LocalDate> dateStream = startDate.datesUntil(endDate);
+        dateStream.forEach(ld -> System.out.print(ld + " "));
+        //
+        System.out.println();
+        System.out.println(localDateTest1.isLeapYear());
+
     }
 }
+
